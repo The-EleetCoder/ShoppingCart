c335380cd02d41c842a1c0aa03e3279ad9345ed5
The-EleetCoder <jaijain1803@gmail.com>
2023-10-08 18:20:18 +0530

    component setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,26 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <NavLink to="/">
+          <div>
+            <img src="" />
+          </div>
+        </NavLink>
+        <div>
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+          <NavLink to='/cart'>
+            <FaShoppingCart />
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
